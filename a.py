8bc2e0661509114331c78cc800abc84551437ab3
yusong <42.4.yusong@gmail.com>
2021-07-15 20:53:46 +0900

    17298

--- a.py
+++ a.py
@@ -1,10 +1,4 @@
 a = [1, 2, 3]
 b = ['a', 'b', 'c']
-def abc(a, x):
-	if x == 3:
-		return
-	x += 1
-	a[0], a[1] = a[1], a[0]
-	abc(a, x)
-abc(b, 1)
-print(b)
+for i in range(10, 9):
+	print(i, end=' ')
