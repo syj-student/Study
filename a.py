3c16687f97530d3562f5f098c0f558fa38453264
yusong <42.4.yusong@gmail.com>
2021-07-19 21:12:40 +0900

    Ongoing

--- a.py
+++ a.py
@@ -1,4 +1,15 @@
-a = [1, 2, 3]
-b = ['a', 'b', 'c']
-for i in range(10, 9):
-	print(i, end=' ')
+def  merge_sort(lst):
+	lstlen = len(lst)
+	if lstlen <= 1:
+		return lst
+	mid = lstlen // 2
+	g1 = merge_sort(lst[:mid])
+	g2 = merge_sort(lst[mid:])
+	ret = list()
+	while g1 and g2:
+		if g1[0] < g2[0]:
+			ret.append(g1.pop[0])
+		else:
+			ret.append(g2.pop[0])
+
+	return ret
