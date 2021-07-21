f603b379b75e05474baf0f975381d49c3b692338
yusong <42.4.yusong@gmail.com>
2021-07-21 21:01:41 +0900

    1874

---
+++ BackJoon/Stack/1874번 스택수열.py
@@ -0,0 +1,25 @@
+import sys
+
+
+n = int(input())
+stack = list()
+ret = list()
+cnt = 1
+flag = 0
+for __ in range(n):
+	x = (int(sys.stdin.readline()))
+	while cnt <= x:
+		stack.append(cnt)
+		ret.append('+')
+		cnt += 1
+	if stack[-1] == x:
+		stack.pop()
+		ret.append('-')
+	else:
+		print('NO')
+		flag = 1
+		break
+
+if flag == 0:
+	for i in ret:
+		print(i)
