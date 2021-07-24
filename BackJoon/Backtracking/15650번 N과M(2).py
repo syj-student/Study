fc1dd3af9942aae06a9743cd5e0247f7cfd72988
yusong <42.4.yusong@gmail.com>
2021-07-25 07:18:51 +0900

    15650

---
+++ BackJoon/Backtracking/15650번 N과M(2).py
@@ -0,0 +1,26 @@
+import sys
+
+# input
+n, m = map(int, sys.stdin.readline().split())
+
+# solve
+answer = set()
+ret = list()
+check = [True] * (n + 1)
+def dfs(n, depth, m):
+	if depth == m:
+		answer.add(tuple(sorted(ret)))
+		return
+	for i in range(1, n + 1):
+		if check[i]:
+			ret.append(i)
+			check[i] = False
+			dfs(n, depth + 1, m)
+			check[i] = True
+			ret.pop()
+
+# print
+dfs(n, 0, m)
+
+for i in sorted(list(answer)):
+	print(*i)
