8cd39eeba432090940338e099180ebe2744898dc
syj <hssong1024@gmail.com>
2021-09-11 19:20:31 +0900

    kakao test

--- tmp.py
+++ tmp.py
@@ -1,21 +1,2 @@
-class LinkedList:
-	def __init__(self, val, next=None):
-		self.val = val
-		self.next = next
-	def listappend(self, val):
-		data = LinkedList(val)
-		while self.next:
-			self = self.next
-		self.next = data
-
-def test(head):
-	head = head.next
-	head = 20
-	print(head)
-
-a = LinkedList(10)
-a.listappend(20)
-
-while a:
-	print(a.val)
-	a = a.next
+if not None or 10 > None:
+	print(10)
--- 파이썬 알고리즘 인터뷰/14. 두 정렬리스트의 병합.py
+++ 파이썬 알고리즘 인터뷰/14. 두 정렬리스트의 병합.py
@@ -20,13 +20,25 @@ class Solution:
 	def mergeTwoLists(self, l1, l2):
 		ret = None
 		while l1 or l2:
-			if not l1 or l1.val >= l2.val:
+			if l1.val >= l2.val:
 				ret = self.__append(ret, l2.val)
 				l2 = l2.next
 			else:
 				ret = self.__append(ret, l1.val)
 				l1 = l1.next
+		tmp = l1 if l1 else l2
+		while tmp:
+			ret = self.__append(ret, tmp.val)
+			tmp = tmp.next
 		return ret
+
+#class Solution:
+#	def mergeTwoLists(self, l1, l2):
+#		if (not l1) or (l2 and l1.val > l2.val):
+#			l1, l2 = l2, l1
+#		if l1:
+#			l1.next = self.mergeTwoLists(l1.next, l2)
+#		return l1
 
 l1 = ListNode(10)
 l2 = ListNode(20)
