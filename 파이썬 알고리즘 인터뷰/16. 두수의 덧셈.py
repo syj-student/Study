dbd9e20ec686240ce19c736c43a5bc4762bf6f94
syj <hssong1024@gmail.com>
2021-09-17 23:32:02 +0900

    nothing

--- 파이썬 알고리즘 인터뷰/16. 두수의 덧셈.py
+++ 파이썬 알고리즘 인터뷰/16. 두수의 덧셈.py
@@ -8,18 +8,8 @@
 
 class Solution:
 	def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-		l1_Number = ''
-		l2_Number = ''
-		while l1 or l2:
-			if not l1:
-				l1_Number += str(l1.val)
-				l1 = l1.next
-			if not l2:
-				l2_Number += str(l2.val)
-				l2 = l2.next
-		ret_Number = list(map(int, reversed(str(int(l1_Number) + int(l2_Number)))))
-		head = tmp
-		for i in ret_Number:
-			tmp = ListNode(i)
-			tmp = tmp.next
+		root = head = ListNode(0)
+
+		carry = 0
+		while l1 or l2 or carry:
 		return head
