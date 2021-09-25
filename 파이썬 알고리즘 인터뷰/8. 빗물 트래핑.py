e8db62d8685a899a9020a8af5357fb3db0ff037a
yusong <42.4.yusong@gmail.com>
2021-09-25 23:12:36 +0900

    파이썬 알고리즘 인터뷰 23번

--- 파이썬 알고리즘 인터뷰/8. 빗물 트래핑.py
+++ 파이썬 알고리즘 인터뷰/8. 빗물 트래핑.py
@@ -1,34 +1,38 @@
 # https://leetcode.com/problems/trapping-rain-water/
 
-class Solution:
-	def trap(self, height: List[int]) -> int:
-		stack = list()
-		volume = 0
-		for i in range(len(height)):
-			while stack and height[i] > height[stack[-1]]:
-				top = stack.pop()
-				if not len(stack):
-					break
-				distance = i - stack[-1] - 1
-				waters = min(height[i], height[stack[-1]]) - height[top]
-				volume += distance * waters
-			stack.append(i)
-		return volume
+#class Solution:
+#	def trap(self, height: List[int]) -> int:
+#		stack = list()
+#		volume = 0
+#		for i in range(len(height)):
+#			while stack and height[i] > height[stack[-1]]:
+#				top = stack.pop()
+#				if not len(stack):
+#					break
+#				distance = i - stack[-1] - 1
+#				waters = min(height[i], height[stack[-1]]) - height[top]
+#				volume += distance * waters
+#			stack.append(i)
+#		return volume
 
 class Solution:
-    def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
-        volume = 0
-        left, right = 0, len(height) - 1
-        left_max, right_max = height[left], height[right]
-        while left < right:
-            left_max = max(left_max, height[left])
-            right_max = max(right_max, height[right])
-            if left_max < right_max:
-                volume += left_max - height[left]
-                left += 1
-            else:
-                volume += right_max - height[right]
-                right -= 1
-        return volume
+	def trap(self, height):
+		if not height:
+			return 0
+		volume = 0
+		left, right = 0, len(height) - 1
+		left_max, right_max = height[left], height[right]
+		while left <= right:
+			left_max = max(left_max, height[left])
+			right_max = max(right_max, height[right])
+			if left_max <= right_max:
+				volume += left_max - height[left]
+				left += 1
+			else:
+				volume += right_max - height[right]
+				right -= 1
+		return volume
+
+a = Solution()
+b = [0, 2, 0, 1, 0, 0]
+print(a.trap(b))
