6144a0c21527a3e39412e06b160e642a67ddaac0
yusong <42.4.yusong@gmail.com>
2021-11-01 01:59:14 +0900

    파이썬 알고리즘 인터뷰 55번

--- DataStructure_Algorithm/Tree/Heap.py
+++ DataStructure_Algorithm/Tree/Heap.py
@@ -1,3 +1,6 @@
+import collections
+
+
 class TreeNode:
 	def __init__(self, val=0, left=None, right=None):
 		self.val = val
@@ -8,15 +11,54 @@ class TreeNode:
 
 class heap:
 	def __init__(self):
-		self.root = None
-		self.len = 0
+		self.tree = [None]
 	
 	def __len__(self):
-		return self.len
+		return len(self.tree) - 1
+
+	def __str__(self):
+		if len(self) == 0:
+			return "None"
+		return str(self.tree[1:])
 
 	def insert(self, val):
-		if self.root is None:
-			self.root = TreeNode(val)
-			self.insert_position = self.root.left
+		self.tree.append(val)
+		child_index = len(self.tree) - 1
+		parent_index = child_index // 2
+		while parent_index:
+			if self.tree[parent_index] > self.tree[child_index]:
+				self.tree[parent_index], self.tree[child_index] = self.tree[child_index], self.tree[parent_index]
+			child_index //= 2
+			parent_index = child_index // 2
+
+	def pop(self):
+		if len(self) == 0:
 			return
-		
+		if len(self) == 1:
+			return self.tree.pop()
+		ret = self.tree[1]
+		self.tree[1] = self.tree.pop()
+		index = 1
+		while True:
+			left, right = index*2, index*2 + 1
+			smallest = index
+			if left <= len(self) and self.tree[left] < self.tree[index]:
+				smallest = left
+			if right <= len(self) and self.tree[right] < self.tree[smallest]:
+				smallest = right
+			if smallest != index:
+				self.tree[smallest], self.tree[index] = self.tree[index], self.tree[smallest]
+				index = smallest
+			else:
+				break
+		return ret
+a = heap()
+b = [10, 5, 8, 7, 7]
+for i in b:
+	a.insert(i)
+print(a)
+print(a.pop(), a)
+print(a.pop(), a)
+print(a.pop(), a)
+print(a.pop(), a)
+print(a.pop(), a)
