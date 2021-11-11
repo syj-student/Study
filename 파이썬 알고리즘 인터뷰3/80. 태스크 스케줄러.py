573f17281a5e408f3822fd4e68cc4cee6dc18398
yusong-offx <42.4.yusong@gmail.com>
2021-11-11 20:49:04 +0900

    파이썬 알고리즘 인터뷰 80번

---
+++ 파이썬 알고리즘 인터뷰3/80. 태스크 스케줄러.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/task-scheduler/
+
+from typing import Collection
+
+
+class Solution:
+	def leastInterval(self, tasks: List[str], n: int) -> int:
+		counter = collections.Counter(tasks)
+		result = 0
+		while True:
+			sub_cnt = 0
+			for task, _ in counter.most_common(n+1):
+				result += 1
+				sub_cnt += 1
+				counter.subtract(task)
+				counter += collections.Counter()
+			if not counter:
+				break
+			result += n + 1 - sub_cnt
+		return result
