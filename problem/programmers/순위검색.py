57b0e71d6c237cff0d201c57b053d774149716dd
yusong-offx <42.4.yusong@gmail.com>
2021-11-17 11:05:46 +0900

    2021 카카오코딩테스트 순위검색

---
+++ problem/programmers/순위검색.py
@@ -0,0 +1,44 @@
+import collections
+import bisect
+
+class node():
+	def __init__ (self):
+		self.score = list()
+		self.next_node = collections.defaultdict(node)
+
+def solution(info, query):
+	def insert(root, cand, score):
+		for c in cand:
+			root = root.next_node[c]
+		root.score.append(score)
+
+
+	def getcnt(root, cand, score, i=0):
+		if i == len(cand):
+			root.score.sort()
+			num = bisect.bisect_left(root.score, score)
+			if num != -1:
+				return len(root.score) - num
+		cnt = 0
+		if cand[i] == '-':
+			for c in root.next_node.keys():
+				cnt += getcnt(root.next_node[c], cand, score, i+1)
+		else:
+			cnt = getcnt(root.next_node[cand[i]], cand, score, i+1)
+		return cnt
+		
+		
+	trie = node()
+	for cand in info:
+		p = cand.split()
+		score = int(p.pop())
+		insert(trie, p, score)
+
+	result = list()
+	for qu in query:
+		tmp = qu.replace(' and', '').split()
+		score = int(tmp.pop())
+		p = getcnt(trie, tmp, score)
+		result.append(p)
+
+	return result
