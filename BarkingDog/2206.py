2c03b2de1bbf2869b9f0a000ac50aa102d6d0038
yusong <hssong1024@gmail.com>
2022-08-01 21:30:15 +0900

    barking dog

--- BarkingDog/2206.py
+++ BarkingDog/2206.py
@@ -1,17 +1,18 @@
 import sys
 import heapq
+from collections import deque
 
 input = sys.stdin.readline
 i, j = map(int, input().split())
 m = list()
 for _ in range(i):
     m.append(list(input().rstrip()))
-h = [(0, 0, 0)]
 move = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-visited = [[[0, 0]] * j for _ in range(i)]
+visited = [[[0, 0] for _ in range(j)] for _ in range(i)]
 visited[0][0][0] = 1
+h = [(1, 0, 0, 0)]
 while h:
-    x, y, z = heapq.heappop(h)
+    _, z, x, y, = heapq.heappop(h)
     if x == i-1 and y == j-1:
         print(visited[x][y][z])
         break
@@ -19,11 +20,11 @@ while h:
         dx = x + a
         dy = y + b
         if 0 <= dx < i and 0 <= dy < j:
-            if m[dx][dy] == '1' and not z == 0:
+            if m[dx][dy] == '1' and z == 0:
                 visited[dx][dy][1] = visited[x][y][0] + 1
-                heapq.heappush(h, (dx, dy, 1))
+                heapq.heappush(h, (visited[x][y][0] + 1, 1, dx, dy,))
             elif m[dx][dy] == '0'  and visited[dx][dy][z] == 0:
                 visited[dx][dy][z] = visited[x][y][z] + 1
-                heapq.heappush(h, (dx, dy, z))
+                heapq.heappush(h, (visited[x][y][z] + 1, z, dx, dy))
 else:
     print(-1)
--- BarkingDog/9466.py
+++ BarkingDog/9466.py
@@ -1,15 +1,16 @@
 import sys
 sys.setrecursionlimit(10 ** 9)
 
-def rec(start, now, road2):
+def rec(start, now, road, road2):
     global vote, visited
-    if now in road2:
+    if now in road:
         return road2[road2.index(now):]
     if visited[now]:
         return list()
     visited[now] = True
+    road.add(now)
     road2.append(now)
-    return rec(start, vote[now], road2)
+    return rec(start, vote[now], road, road2)
 
 input = sys.stdin.readline
 n = int(input())
@@ -20,6 +21,6 @@ for _ in range(n):
     have_group = list()
     for start in range(persons):
         if not visited[start]:
-            have_group.append(rec(start, start, list()))
+            have_group.append(rec(start, start, set(), list()))
     print(persons - len(have_group))
 
