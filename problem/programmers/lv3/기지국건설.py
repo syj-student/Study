b95c5c359350c58f1e3eb720b68651bdb8603a97
yusong <hssong1024@gmail.com>
2022-09-18 12:39:58 +0900

    프로그래머스 lv3

---
+++ problem/programmers/lv3/기지국건설.py
@@ -0,0 +1,30 @@
+import math
+
+def solution(n, stations, w):
+    answer = 0
+    repeater = set()
+    stations.sort()
+    rg = list()
+    for i in stations:
+        i -= 1
+        repeater.add(i)
+        start = i-w if i-w >= 0 else 0
+        end = i+w if i+w < n else n
+        if rg and rg[-1][1] >= start:
+            rg[-1][1] = end
+        else:
+            rg.append([start, end])
+    dis = list()
+    prev = 0
+    for s, e in rg:
+        dis.append(s - prev)
+        prev = e+1
+    if prev < n:
+        dis.append(n - prev)
+    for d in dis:
+        answer += math.ceil(d / (w*2+1))
+    return answer
+
+print(solution(
+    16, [], 2
+))
--- tmp2.py
+++ tmp2.py
@@ -1,4 +1,25 @@
-import bisect
-from collections import deque
-a = deque([1, 0])
-print(bisect.bisect_right(a, 1))
+def solution(n, stations, w):
+    answer = 0
+    repeater = set(map(lambda x: x-1, stations))
+    apt = [False] * n
+    i = 0
+    while i < n:
+        if apt[i] == False:
+            tmp = i+w
+            print(tmp)
+            if tmp not in repeater:
+                answer += 1
+            i = tmp + w + 1
+        else:
+            if i in repeater:
+                i = i+w+1
+            else:
+                i += 1
+
+    return answer
+
+print(
+    solution(
+        16, [9], 2
+    )
+)
